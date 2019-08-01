078bedff90863a30213f43bc559b85fdab9a4be6
babinkuk <nikola.babic@gmail.com>
2019-08-01 10:49:05 +0200

    setup prod build

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,34 @@ console.log(path.join(__dirname, 'public'));
 
 // entry -> output
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, // check for .js file extension 
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/, // check for .css/.scss file extension
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+module.exports = (env) => {
+    console.log('env :', env);
+    // check env
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, // check for .js file extension 
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/, // check for .css/.scss file extension
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    };
 };
